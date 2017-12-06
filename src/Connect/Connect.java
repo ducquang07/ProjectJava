b56691e26e33e1fc28480fc279a5626c2926ba30
Thái Nguyễn <15520783@gm.uit.edu.vn>
2017-12-06 20:05:14 +0700

    add Connect class

---
+++ src/Connect/Connect.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ThaiNguyen
+ */
+public class Connect {
+    private Connection conDB=null;
+    private Statement stmDB=null;
+    private ResultSet rsDB=null;
+    
+    private final String servername="closer.heliohost.org:3306";
+    private final String databasename="closer_quanlicuahangson";
+    private final String username="closer";
+    private final String password="Bo4quyennang";
+    private String url="";
+    
+    public boolean Connected(){
+        try{
+            if((conDB!=null)&&(!conDB.isClosed())){
+                conDB.close();
+            }
+            url="jdbc:mysql://"+servername+"/"+databasename+"?characterEncoding=utf8";
+            conDB=DriverManager.getConnection(url,username,password);
+        }
+        catch(SQLException ex){
+            System.out.println("Ngoại lệ tại Connect.Connected :"+ex.getMessage());
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean CloseDB(){
+        try{
+            if(!conDB.isClosed()){
+                conDB.close();
+            }
+        }
+        catch(SQLException ex){
+            System.out.println("Ngoại lệ tại Connect.Closed :"+ex.getMessage());
+            return false;
+        }
+        return true;
+    }
+    
+    public ResultSet GetData(String SQL){
+        ResultSet rs = null;
+        try{
+            if(Connected()){
+                stmDB =(Statement) conDB.createStatement();
+                rs=stmDB.executeQuery(SQL);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Ngoại lệ tại Connect.GetData: "+ex.getMessage());
+        }
+        finally{
+            CloseDB();
+        }
+        return rs;
+    }
+    
+     public static void main(String args[]) { 
+         Connect cn = new Connect();
+         if(cn.Connected())
+             System.out.println("Connected");
+     }
+
+}
